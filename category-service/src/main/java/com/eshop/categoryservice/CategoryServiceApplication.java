2454e141797c98e169c860b3b98266242b442848
Ciobotaru Mihai <mihai2096@gmail.com>
2025-06-20 21:41:00 +0300

    added product & category service and refactoring category

---
+++ category-service/src/main/java/com/eshop/categoryservice/CategoryServiceApplication.java
@@ -0,0 +1,11 @@
+package com.eshop.categoryservice;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryServiceApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(CategoryServiceApplication.class, args);
+    }
+    
+}
